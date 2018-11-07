9741d7ed2e561e80aeea3ea1fd7312b394f5f2d4
KristapsTaube <kristaps.taube@printful.com>
2018-11-07 10:24:04 +0200

    Product update examples. SyncProductParameters removed and refactored usage

--- src/Structures/Sync/SyncProductUpdateParameters.php
+++ src/Structures/Sync/SyncProductUpdateParameters.php
@@ -5,11 +5,35 @@ namespace Printful\Structures\Sync;
 use Printful\Structures\Sync\Requests\SyncProductRequest;
 use Printful\Structures\Sync\Requests\SyncVariantRequest;
 
-class SyncProductUpdateParameters extends SyncProductParameters
+class SyncProductUpdateParameters
 {
     /** @var SyncProductRequest|null */
     public $syncProduct;
 
     /** @var SyncVariantRequest[]|null */
     public $syncVariants;
+
+    /**
+     * Builds SyncProductUpdateParameters from array
+     *
+     * @param array $array
+     * @return SyncProductUpdateParameters
+     */
+    public static function fromArray(array $array)
+    {
+        $params = new SyncProductUpdateParameters;
+
+        if(isset($array['sync_product'])){
+            $params->syncProduct = SyncProductRequest::fromArray($array['sync_product']);
+        }
+
+        if(isset($array['sync_variants'])){
+            foreach ($array['sync_variants'] as $item) {
+                $syncVariantRequest = SyncVariantRequest::fromArray($item);
+                $params->syncVariants[] = $syncVariantRequest;
+            }
+        }
+
+        return $params;
+    }
 }
